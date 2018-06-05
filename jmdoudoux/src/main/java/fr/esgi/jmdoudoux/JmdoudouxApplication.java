26cd39ccb7f1029689358e453bff1c758c92c5a5
Darkkrye <pierre.boudon.27@gmail.com>
2018-06-05 12:22:44 +0200

    Added Spring Project

---
+++ jmdoudoux/src/main/java/fr/esgi/jmdoudoux/JmdoudouxApplication.java
@@ -0,0 +1,12 @@
+package fr.esgi.jmdoudoux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmdoudouxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmdoudouxApplication.class, args);
+	}
+}
---
+++ jmdoudoux/src/test/java/fr/esgi/jmdoudoux/JmdoudouxApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.esgi.jmdoudoux;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JmdoudouxApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
