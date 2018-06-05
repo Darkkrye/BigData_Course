ff77e4e0ab0ef23b79aff1cdda224e9aa707a124
Darkkrye <pierre.boudon.27@gmail.com>
2018-06-05 12:15:26 +0200

    Initial Commit

--- Course/demo/src/main/java/fr/esgi/demo/Main.java
+++ Course/demo/src/main/java/fr/esgi/demo/Main.java
@@ -3,6 +3,7 @@ package fr.esgi.demo;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.mllib.stat.Statistics;
 import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SQLContext;
 
 /**
@@ -11,11 +12,18 @@ import org.apache.spark.sql.SQLContext;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final SQLContext context = LocalContexts.get().getSqlSession().sqlContext();
         final CsvLoader loader = new CsvLoader(context);
         Dataset<Cours> coursCAC40 = loader.loadDataset(LocalContexts.get().getCac40Path());
         Dataset<Cours> coursSG = loader.loadDataset(LocalContexts.get().getSGPath());
+        coursSG.createTempView("COURSSG");
+        long value = coursSG.sqlContext().sql("select count (*) from COURSSG").first().getLong(0);
+        System.out.println("J’ai des valeurs !! \t" + value);
+        Dataset<Row> dates = coursSG.sqlContext().sql("select date from COURSSG");
+        System.out.println("Début écriture");
+        dates.write().csv("~/Documents/test.csv");
+        System.out.println("Fin écriture");
         JavaRDD<Double> clotureCAC40 = coursCAC40.javaRDD().map((Cours c) -> c.getCloture());
         JavaRDD<Double> clotureSG = coursSG.javaRDD().map((Cours c) -> c.getCloture());
         double correlations = Statistics.corr(clotureSG, clotureCAC40);
