4fad9c6bdad6eb06c27f6eb0826dadeac5fb3391
Salomé <salome.russier@gmail.com>
2018-06-06 16:05:22 +0200

    fix gradle setup

--- JMDoudoux_Android/app/src/main/java/com/example/esgi/jmdoudoux_android/MainActivity.java
+++ JMDoudoux_Android/app/src/main/java/com/example/esgi/jmdoudoux_android/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.esgi.jmdoudoux_android;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatTextView;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements INGram{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e("Main", "begining");
+
+
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        RetrofitHelper.getNGram("Paris", this);
+    }
+
+    @Override
+    public void onRetrofitResult(boolean okay) {
+        if(okay){
+            Log.e("MAIN", "ok");
+        }else{
+            Log.e("MAIN", "ko");
+        }
     }
 }
