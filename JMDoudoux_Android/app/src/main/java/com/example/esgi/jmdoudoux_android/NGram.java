2219a228d0db84cfd0063d13a145c516f443cee0
Meryl Barantal <meryl2194@gmail.com>
2018-06-07 08:44:57 +0200

    add algo

--- JMDoudoux_Android/app/src/main/java/com/example/esgi/jmdoudoux_android/NGram.java
+++ JMDoudoux_Android/app/src/main/java/com/example/esgi/jmdoudoux_android/NGram.java
@@ -5,7 +5,31 @@ import java.util.ArrayList;
 public class NGram {
 
     private ArrayList<Gram> grams;
-    private Long n;
+    private int n;
 
+    public NGram() {
+        this.grams = new ArrayList<>();
+        this.n = 0;
+    }
 
+    public NGram(ArrayList<Gram> grams, int n) {
+        this.grams = grams;
+        this.n = n;
+    }
+
+    public ArrayList<Gram> getGrams() {
+        return grams;
+    }
+
+    public void setGrams(ArrayList<Gram> grams) {
+        this.grams = grams;
+    }
+
+    public int getN() {
+        return n;
+    }
+
+    public void setN(int n) {
+        this.n = n;
+    }
 }
